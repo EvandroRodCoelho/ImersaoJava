863c0c36739a8ba117edce16339fa7d1685c83d3
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-03-25 23:18:54 -0300

    Feat:Http connect

--- src/App.java
+++ src/App.java
@@ -1,6 +1,17 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 public class App {
+
+
     public static void main(String[] args) throws Exception {
-        String url = "https://api.github.com/users/EvandroRodCoelho";
-        
+        String url = "https://gist.githubusercontent.com/lucasfugisawa/cbb0d10ee3901bd0541468e431c629b3/raw/1fe1f3340dfe5b5876a209c0e8226d090f6aef10/Top250Movies.json";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String json = response.body();
+        System.out.println(json);
     }
 }

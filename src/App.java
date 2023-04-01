6ab4f68a844f98b36f3f0f343ec26fb7a9308a7d
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2023-04-01 14:35:51 -0300

    Refactoring: POO

--- src/App.java
+++ src/App.java
@@ -1,41 +1,37 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
+
+import services.ClientHttp;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI address = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(address).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
-
+        String ImdbUrl = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        String NasaUrl = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
         // extrair só os dados que interessam (titulo, poster, classificação)
-        var parser = new JsonParser();
-        List<Map<String, String>> movieList = parser.parse(body);
+        var http = new ClientHttp();
+        String json  = http.getDatas(NasaUrl);
+
+
 
         // exibir e manipular os dados 
-        for (Map<String, String> movie : movieList) {
-            String urlImage = movie.get("image");
-            String title = movie.get("title");
-            
-            InputStream inputStream = new URL(urlImage).openStream();
-            String archiveName ="assets/exit/" + title + ".png";
+
+        var extractor = new NasaContentExtractor();
+        List<Content> contents = extractor.extractContent(json);
+        
+        for (int i = 0; i < 3; i++) {
+            Content content = contents.get(i);
+
+            InputStream inputStream = new URL(content.getImageUrl()).openStream();
+            String archiveName ="assets/exit/" + content.getTitle() + ".png";
 
             var generate = new StickerGenerator();
       
-
             generate.createSticker(inputStream, archiveName);
-            System.out.println(title);
+            System.out.println(content.getTitle());
         }
     }
 }
